c82704af1599cf3b038de8fd9d2cb394b1a4394f
grillsnusk <grillsnusk@gmail.com>
2016-01-30 12:32:24 +0100

    ASDF

--- Ritual/Ritual.h
+++ Ritual/Ritual.h
@@ -1,12 +1,27 @@
 #pragma once
 
 #include "include\SDL.h"
+#include "lodepng.h"
 #include <vector>
 #include <hash_map>
 #include <hash_set>
 #include <list>
 #include <algorithm>
 using namespace std;
+
+typedef float					f32;
+typedef	double					f64;
+typedef signed		char		i8;
+typedef unsigned	char		u8;
+typedef signed		short		i16;
+typedef unsigned	short		u16;
+typedef signed		int			i32;
+typedef unsigned	int			u32;
+typedef signed		__int64		i64;
+typedef unsigned	__int64		u64;
+typedef signed		long long	i64;
+typedef unsigned	long long	u64;
+
 
 struct unit;
 struct tower;
