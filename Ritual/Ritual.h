890522d9c4dc8069404775de501c60a3f44e2d62
Robert Toth <robert.m.toth@gmail.com>
2016-01-30 11:56:45 +0100

    added some structs

--- Ritual/Ritual.h
+++ Ritual/Ritual.h
@@ -1,5 +1,41 @@
 #pragma once
 
 #include "include\SDL.h"
+#include <vector>
+#include <hash_map>
+#include <hash_set>
+#include <list>
+#include <algorithm>
+using namespace std;
 
+struct unit;
+struct tower;
 
+struct interaction {
+	float t;
+	unit *unit;
+	tower *tower;
+};
+
+struct position {
+	float t;
+	float x, y;
+};
+
+struct health {
+	float t;
+	float hp;
+	interaction *i; // What caused the change
+};
+
+struct unit {
+	float spawntime;
+	vector<position> path;
+	vector<health> hp;
+};
+
+struct tower {
+	float spawntime;
+	int x, y;
+	vector<interaction*> shots;
+};
